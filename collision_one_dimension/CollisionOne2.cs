03ead61cc269fcfa150e5e4bed9fa2073083ee59
jordane-li <69186130+jordane-li@users.noreply.github.com>
2021-01-20 20:42:26 +0800

    c# scripts for movement
    These are the main scripts controling the movement of the object, including the scene management in Unity.

---
+++ collision_one_dimension/CollisionOne2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionOne2 : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    //speed用于保存碰撞前的速度，v用于计算碰撞后的速度
+    private float mass1, mass2, speed1, speed2, v1, v2;
+
+    // Start is called before the first frame updat
+    void Start()
+    {
+        //将物体设为蓝色
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    private void FixedUpdate()
+    {
+        //获取物体的质量
+        mass1 = GameObject.Find("Cube1").GetComponent<Rigidbody>().mass;
+        mass2 = GameObject.Find("Cube2").GetComponent<Rigidbody>().mass;
+        //获取物体速度
+        Vector3 Speed1 = GameObject.Find("Cube1").GetComponent<Rigidbody>().velocity;
+        Vector3 Speed2 = GameObject.Find("Cube2").GetComponent<Rigidbody>().velocity;
+
+        speed1 = Vector3.Dot(Speed1, Vector3.right);
+        speed2 = Vector3.Dot(Speed2, Vector3.right);
+
+        v1 = speed1;
+        v2 = speed2;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        //打印碰撞到物体的名称
+        Debug.Log(collision.gameObject.name);
+        //获取碰撞到物体的Render组件并且更改其材质颜色
+        collision.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        //计算碰撞后的速度
+        v1 = (mass1 * speed1 + mass2 * speed2) / (mass1 + mass2);
+        v2 = (mass1 * speed1 + mass2 * speed2) / (mass1 + mass2);
+        Debug.Log(v1);
+        Debug.Log(v2);
+        if (this.name == "Cube1")
+        {
+            MoveSpeed = v1;
+        }
+        else
+        {
+            MoveSpeed = v2;
+        }
+        this.GetComponent<Rigidbody>().velocity = Vector3.right * MoveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(900, 0, 200, 50), "物体1的质量:" + mass1.ToString("f1"));
+        GUI.Label(new Rect(900, 20, 200, 50), "物体2的质量:" + mass2.ToString("f1"));
+        GUI.Label(new Rect(900, 40, 200, 50), "物体1的速度:" + v1.ToString("f1"));
+        GUI.Label(new Rect(900, 60, 200, 50), "物体2的速度:" + v2.ToString("f1"));
+    }
+}
---
+++ scene_manage/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private bool condition = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!condition)
+        {
+            SceneManager.LoadScene(Begin.scene_name, LoadSceneMode.Additive);
+            condition = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
